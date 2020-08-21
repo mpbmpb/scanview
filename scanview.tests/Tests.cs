1f4c3bcd47e6f94b54703d7dbbb0824b7df987e1
mpbmpb <markkeyster@gmail.com>
2020-08-21 16:50:26 +0200

    refactored some, halfway through refactoring sorting logic

--- scanview.tests/Tests.cs
+++ scanview.tests/Tests.cs
@@ -11,40 +11,40 @@ namespace scanview.tests
         //followed by all larger than controller entities and smaller than subject entities
 
         [Theory]
-        [InlineData("Subject", "Index", new string[] { "Day", "Subject" })]
-        [InlineData("Contact", "Index", new string[] {"Contact"})]
-        [InlineData("CourseDate", "Edit", new string[] {"Course", "CourseDate", "Venue"})]
-        [InlineData("CourseDesign", "Index", new string[] {"CourseDesign", "Subject"})]
-        [InlineData("CourseDesign", "Create", new string[] {"CourseDesign", "Day"})]
-        public void FindEntities_returns_controller_and_smallest_subEntity_and_entities_outside_this_range_in_controller_View(string controller, string view, string[] expected)
+        [InlineData("Subject/Index", new string[] { "Day", "Subject" })]
+        [InlineData("Contact/Index", new string[] {"Contact"})]
+        [InlineData("CourseDate/Edit", new string[] {"Course", "CourseDate", "Venue"})]
+        [InlineData("CourseDesign/Index", new string[] {"CourseDesign", "Subject"})]
+        [InlineData("CourseDesign/Create", new string[] {"CourseDesign", "Day"})]
+        public void FindEntities_returns_controller_and_smallest_subEntity_and_entities_outside_this_range_in_controller_View(string controllerview, string[] expected)
         {
-            var result = Program.FindEntities(controller, view);
+            var result = Extensions.GetRelevantEntities(controllerview);
 
             result.Should().Contain(expected);
             result.Should().HaveCount(expected.Length);
         }
 
         [Theory]
-        [InlineData("Subject", "Index", new string[] { "Subject", "Day" })]
-        [InlineData("Subject", "Edit", new string[] { "Subject"})]
-        [InlineData("Day", "Index", new string[] { "Day", "Subject"})]
-        [InlineData("Day", "Details", new string[] { "Day", "Subject"})]
-        [InlineData("Seminar", "Index", new string[] { "Seminar", "Day" })]
-        [InlineData("Seminar", "Create", new string[] { "Seminar", "Subject" })]
-        [InlineData("Seminar", "Edit", new string[] { "Seminar", "Subject" })]
-        [InlineData("CourseDesign", "Index", new string[] { "CourseDesign", "Subject" })]
-        [InlineData("CourseDesign", "Create", new string[] { "CourseDesign", "Day" })]
-        [InlineData("CourseDate", "Create", new string[] { "Course", "CourseDate", "Venue" })]
-        [InlineData("CourseDate", "Edit", new string[] { "Course", "CourseDate", "Venue" })]
-        [InlineData("Course", "Index", new string[] { "Course", "Subject" })]
-        [InlineData("Course", "Details", new string[] { "Course", "Subject" })]
-        [InlineData("Course", "Edit", new string[] { "Course", "CourseDesign" })]
-        [InlineData("Course", "Create", new string[] { "Course", "CourseDesign" })]
-        [InlineData("Venue", "Create", new string[] { "Venue" })]
-        [InlineData("Venue", "Details", new string[] { "Venue" })]
-        public void FindEntities_returns_entities_in_proper_order(string controller, string view, string[] expected)
+        [InlineData("Subject/Index", new string[] { "Subject", "Day" })]
+        [InlineData("Subject/Edit", new string[] { "Subject"})]
+        [InlineData("Day/Index", new string[] { "Day", "Subject"})]
+        [InlineData("Day/Details", new string[] { "Day", "Subject"})]
+        [InlineData("Seminar/Index", new string[] { "Seminar", "Day" })]
+        [InlineData("Seminar/Create", new string[] { "Seminar", "Subject" })]
+        [InlineData("Seminar/Edit", new string[] { "Seminar", "Subject" })]
+        [InlineData("CourseDesign/Index", new string[] { "CourseDesign", "Subject" })]
+        [InlineData("CourseDesign/Create", new string[] { "CourseDesign", "Day" })]
+        [InlineData("CourseDate/Create", new string[] { "Course", "CourseDate", "Venue" })]
+        [InlineData("CourseDate/Edit", new string[] { "Course", "CourseDate", "Venue" })]
+        [InlineData("Course/Index", new string[] { "Course", "Subject" })]
+        [InlineData("Course/Details", new string[] { "Course", "Subject" })]
+        [InlineData("Course/Edit", new string[] { "Course", "CourseDesign" })]
+        [InlineData("Course/Create", new string[] { "Course", "CourseDesign" })]
+        [InlineData("Venue/Create", new string[] { "Venue" })]
+        [InlineData("Venue/Details", new string[] { "Venue" })]
+        public void FindEntities_returns_entities_in_proper_order(string controllerview, string[] expected)
         {
-            var result = Program.FindEntities(controller, view);
+            var result = Extensions.GetRelevantEntities(controllerview);
           
             result.Should().Equal(expected);
         }
--- scanview/Program.cs
+++ scanview/Program.cs
@@ -3,66 +3,116 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
+
 namespace scanview
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            var result = FindEntities("Venue", "Index");
+            var result = Extensions.GetRelevantEntities("Course/Index");
             result.ForEach(Console.WriteLine);
-        }
+        }       
+    }
 
-
+    public static class Extensions
+    {
         private static readonly string[] entities = new string[]
         {
-            "Course", "CourseDesign", "Seminar", "Day", "Subject", "CourseDate", "Venue", "Contact"
+            "Course", "CourseDates", "CourseDesign", "CourseSeminars", "Seminar", "SeminarDays", "Day", "DaySubjects", "Subject", "CourseDate", "Venue", "Contact"
         };
 
-        //TODO violates srp, should just find -> create sort function & returnEntities should be main function that calls the others
-        public static List<string> FindEntities(string controllerName, string viewName)
+        private static readonly string[][] navigationRelations = new string[][]
+        {
+            new string [] { "Course", "CourseDesign", "CourseSeminars", "Seminar", "SeminarDays", "Day", "DaySubjects", "Subject" },
+            new string [] { "Course", "CourseDates", "CourseDate", "Venue", "Contact" }
+        };
+
+        //TODO  violates srp, should just find
+        //      create sort function & returnEntities should be main function that calls the others
+        //                                          should be One param controllerViewName i.e. "Day/Index"
+
+        public static List<string> GetRelevantEntities(string controllerViewName)
+        {
+            return FindEntities(controllerViewName);
+
+        }
+
+        public static List<string> FindEntities(string controllerViewName)
         {
             List<string> result = new List<string>();
-            string[] hits = new string[entities.Length];
-            string view = File.ReadAllText($"../../../../scanview/Views/{controllerName}/{viewName}.cshtml");
-         
-            for (int current = 0; current < entities.Length; current++)
+            string view = File.ReadAllText($"../../../../scanview/Views/{controllerViewName}.cshtml");
+
+            foreach (var entity in entities)
             {
-                if (VariationsOf(entities[current]).Any(view.Contains)) { hits[current] = entities[current]; }
-            }                                               // TODO make ContainsAny ext method
-            
-            int smallestEntityInChain = Array.IndexOf(entities, "Subject");
-            int controllerIndex = Array.IndexOf(entities, controllerName);
-            if (controllerIndex <= smallestEntityInChain)
-            {
-                bool smallerEntityFound = false;
-                result.Add(hits[controllerIndex]);
-                hits[controllerIndex] = null;
-                for (int index = smallestEntityInChain; index > controllerIndex; index--)
-                {
-                    if (smallerEntityFound) { hits[index] = null; }
-                    if (hits[index] != null && !smallerEntityFound)
-                    {
-                        result.Add(hits[index]);
-                        hits[index] = null;
-                        smallerEntityFound = true;
-                    }
-                }
+                if (view.ContainsAny(VariationsOf(entity)))
+                    result.Add(entity);
             }
-            
-            foreach (var hit in hits) { if (hit != null) { result.Add(hit); } }
             return result;
         }
 
         private static List<string> VariationsOf(string entity)
         {
-            return new List<string> {$".{entity}.", $".{entity} ", $".{entity}\r\n",
+            return new List<string> {$".{entity}.", $".{entity} ", $".{entity})", $".{entity}\r\n",
                 $".{entity}\r", $".{entity}\n", $".{entity}\"", $".{entity}>", $"<{entity}>",
                 $"ViewBag.{entity}Id", $"ViewData.{entity}Id", $"@model {entity} ",
                 $"@model {entity}\r\n", $"@model {entity}\r", $"@model {entity}\n",
                 $"type=\"hidden\" asp-for=\"{entity}Id\"", $"type=\"hidden\" asp-for=\"{entity}.",
-                $"type=\"hidden\" asp-for=\"{entity}\""};
+                $"type=\"hidden\" asp-for=\"{entity}\""}; //should regex this
+        }
+        public static bool ContainsAny(this string input, List<string> searchStrings)
+        {
+            foreach (string searchTerm in searchStrings)
+                if (input.Contains(searchTerm)) return true;
+            return false;
         }
 
+        public static int[] GetRowNumberFor(this string[][] input, string searchTerm)
+        {
+            int[] result = new int[input.Length];
+            for (int row = 0; row < input.Length; row++)
+                if (input[row].Contains(searchTerm)) result[row] = 1;
+            return result;
+        }
+
+        // if entity has pluralform substring then belongs to entity to the left of said plural
+        public static List<string> ReturnGrouped(this List<string> list)
+        {
+            List<string> result = new List<string>();
+            foreach (string entity in list)
+            {
+                //find index of your row(s), if string with corresponding index in result is empty create new string
+                //else check previous entry in string and complete chain (add yourself and any missing intermediates)
+                navigationRelations.GetRowNumberFor(entity);
+            }
+            //check if string in result consists of 1 entity that entity is unique else remove that string
+            //substring(.entity or entity.) or exact match
+        }
     }
 }
+
+//TODO code smell too many conditionals..polymorphism?
+
+//            string[] hits = new string[entities.Length];
+
+//int smallestEntityInChain = Array.IndexOf(entities, "Subject");
+//int controllerIndex = Array.IndexOf(entities, controllerName);
+//if (controllerIndex <= smallestEntityInChain)
+//{
+//    bool smallerEntityFound = false;
+//    result.Add(hits[controllerIndex]);
+//    hits[controllerIndex] = null;
+//    for (int index = smallestEntityInChain; index > controllerIndex; index--)
+//    {
+//        if (smallerEntityFound) { hits[index] = null; }
+//        if (hits[index] != null && !smallerEntityFound)
+//        {
+//            result.Add(hits[index]);
+//            hits[index] = null;
+//            smallerEntityFound = true;
+//        }
+//    }
+//}
+
+//foreach (var hit in hits) { if (hit != null) { result.Add(hit); } }
+//return result;
